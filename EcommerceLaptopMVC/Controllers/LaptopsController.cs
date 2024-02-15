10bebd3ffa1391f0187b40a8989aebd2c8ab1b27
RagendhuRamesh <ragendhuram@gmail.com>
2024-02-15 21:52:29 +0530

    Add project files.

---
+++ EcommerceLaptopMVC/Controllers/LaptopsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceLaptopMVC.Models;
+
+namespace EcommerceLaptopMVC.Controllers
+{
+    public class LaptopsController : Controller
+    {
+        private ProductsDbEntities db = new ProductsDbEntities();
+
+        // GET: Laptops
+        public ActionResult Index()
+        {
+            var laptops = db.Laptops.Include(l => l.ModelType).Include(l => l.Supplier);
+            return View(laptops.ToList());
+        }
+
+        // GET: Laptops/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laptop laptop = db.Laptops.Find(id);
+            if (laptop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laptop);
+        }
+
+        // GET: Laptops/Create
+        public ActionResult Create()
+        {
+            ViewBag.Model_Id = new SelectList(db.ModelTypes, "Mid", "TypeNAme");
+            ViewBag.Supplier_Id = new SelectList(db.Suppliers, "SupId", "SupName");
+            return View();
+        }
+
+        // POST: Laptops/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Lid,LName,LCore,LSpace,LPrice,MsOffce,Model_Id,Supplier_Id,Price")] Laptop laptop)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Laptops.Add(laptop);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Model_Id = new SelectList(db.ModelTypes, "Mid", "TypeNAme", laptop.Model_Id);
+            ViewBag.Supplier_Id = new SelectList(db.Suppliers, "SupId", "SupName", laptop.Supplier_Id);
+            return View(laptop);
+        }
+
+        // GET: Laptops/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laptop laptop = db.Laptops.Find(id);
+            if (laptop == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Model_Id = new SelectList(db.ModelTypes, "Mid", "TypeNAme", laptop.Model_Id);
+            ViewBag.Supplier_Id = new SelectList(db.Suppliers, "SupId", "SupName", laptop.Supplier_Id);
+            return View(laptop);
+        }
+
+        // POST: Laptops/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Lid,LName,LCore,LSpace,LPrice,MsOffce,Model_Id,Supplier_Id,Price")] Laptop laptop)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(laptop).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Model_Id = new SelectList(db.ModelTypes, "Mid", "TypeNAme", laptop.Model_Id);
+            ViewBag.Supplier_Id = new SelectList(db.Suppliers, "SupId", "SupName", laptop.Supplier_Id);
+            return View(laptop);
+        }
+
+        // GET: Laptops/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laptop laptop = db.Laptops.Find(id);
+            if (laptop == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laptop);
+        }
+
+        // POST: Laptops/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Laptop laptop = db.Laptops.Find(id);
+            db.Laptops.Remove(laptop);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EcommerceLaptopMVC/Controllers/ModelTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceLaptopMVC.Models;
+
+namespace EcommerceLaptopMVC.Controllers
+{
+    public class ModelTypesController : Controller
+    {
+        private ProductsDbEntities db = new ProductsDbEntities();
+
+        // GET: ModelTypes
+        public ActionResult Index()
+        {
+            return View(db.ModelTypes.ToList());
+        }
+
+        // GET: ModelTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ModelType modelType = db.ModelTypes.Find(id);
+            if (modelType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelType);
+        }
+
+        // GET: ModelTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ModelTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Mid,TypeNAme,NOLap")] ModelType modelType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ModelTypes.Add(modelType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(modelType);
+        }
+
+        // GET: ModelTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ModelType modelType = db.ModelTypes.Find(id);
+            if (modelType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelType);
+        }
+
+        // POST: ModelTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Mid,TypeNAme,NOLap")] ModelType modelType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(modelType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(modelType);
+        }
+
+        // GET: ModelTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ModelType modelType = db.ModelTypes.Find(id);
+            if (modelType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(modelType);
+        }
+
+        // POST: ModelTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ModelType modelType = db.ModelTypes.Find(id);
+            db.ModelTypes.Remove(modelType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
